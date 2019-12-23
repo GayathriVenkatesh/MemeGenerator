8cfd23d231251de4fdc70f1547a1709e88e341aa
Gayathri Venkatesh <gayaa2010@gmail.com>
2019-12-23 14:02:37 +0530

    Add files via upload

---
+++ build/precache-manifest.2fee569298ccdb7e478781f8cbd9c698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c29d6f85702ff479ec8bb2f2ae57da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd21308e280b70647992",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93fde0f899ba2a0133ec",
+    "url": "/static/js/2.12658d90.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.12658d90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd21308e280b70647992",
+    "url": "/static/js/main.244e74fa.chunk.js"
+  },
+  {
+    "revision": "e785983f84df9593aae9",
+    "url": "/static/js/runtime-main.c5739dd5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
